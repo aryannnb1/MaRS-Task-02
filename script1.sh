d8cdb36116e2d76ad991d14f92d923fc768d48a0
Aryan Bhardwaj <136265589+aryannnb1@users.noreply.github.com>
2023-06-12 14:38:17 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-
+#takiing directory input
 directory=${pwd}
+#printing the directory
 echo $directory
+#making new directory
 mkdir Modified_dir
 
 file=${grep/.txt}
 
+#loop to check for txt files and creating .bak files from them
 for i in *.txt; do
-     mv "$i" "${i%.txt}.pdf"
+     mv "$i" "${i%.txt}.bak"
 done
-for i in *.pdf; do
+#loop to check for bak files and copying them to modified directory
+for i in *.bak; do
      cp $i Modified_dir/
 done
-for i in *.pdf; do
-     mv "$i" "${i%.pdf}.txt"
+#moving txt files
+for i in *.bak; do
+     mv "$i" "${i%.bak}.txt"
 done

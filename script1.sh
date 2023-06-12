15cc332ea24d309bf69e05460c913f251b066c80
Aryan Bhardwaj <136265589+aryannnb1@users.noreply.github.com>
2023-06-12 14:33:07 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# Function to recursively search for files with .txt extension and copy them to the Modified directory
-function copy_files() {
-  local dir=$1
-  local modified_dir="$dir/Modified"
+directory=${pwd}
+echo $directory
+mkdir Modified_dir
 
-  # Check if the Modified directory exists, if not, create it
-  if [ ! -d "$modified_dir" ]; then
-    mkdir "$modified_dir"
-  fi
+file=${grep/.txt}
 
-  # Find all files with .txt extension and copy them to the Modified directory with .bak extension
-  find "$dir" -type f -name "*.txt" -exec cp {} "$modified_dir"/ \;
-  for file in "$modified_dir"/*.txt; do
-    mv "$file" "${file%.txt}.bak"
-  done
-  echo "Files with .txt extension copied to the Modified directory with .bak extension."
-}
-
-# Prompt the user to enter the current working directory
-read -p "Enter the Current Working Directory: " cwd
-
-# Call the copy_files function with the provided directory as input
-copy_files "$cwd"
-
+for i in *.txt; do
+     mv "$i" "${i%.txt}.pdf"
+done
+for i in *.pdf; do
+     cp $i Modified_dir/
+done
+for i in *.pdf; do
+     mv "$i" "${i%.pdf}.txt"
+done
